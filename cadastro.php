67f3bc985b391e2b4ed5c5f03ca069fb1f088c1f
Juliana <quionesnoww@gmail.com>
2018-06-21 21:24:26 -0300

    Pagina de Cadastro

---
+++ cadastro.php
@@ -0,0 +1,24 @@
+<?php
+
+	$titulo = "Cadastro";
+
+?>
+<html>
+<head>
+	<title><?php echo $titulo; ?></title>
+	<meta chasert= "UTF-8" />
+</head>
+<body>
+	<h1>Cadastro</h1>
+	<form method = "post" action = "cadastro.php">
+		<label>E-mail</label>
+		<input type = "text" name = "email" id = "email" />
+		<br />
+		<input type = "submit" value = "Cadastrar" name = "email" id = "email" />
+	</form>
+<?php
+if($_POST && !empty($_POST['email'])) {
+ echo "Cadastrado com sucesso";
+}	
+</body>	
+	
