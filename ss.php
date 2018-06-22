6477d0d717b5d23cecf725a23ad16f2e3ddbad47
Juliana <quionesnoww@gmail.com>
2018-06-21 21:07:13 -0300

    Inicio Projeto PHP

---
+++ ss.php
@@ -0,0 +1,19 @@
+<?php
+
+	$titulo = "Titulo da Pagina";
+	$tituloConteudo = "<h1>Titulo do texto";
+	$conteudo = "<p>GIT - vERSIONAMENTO</p>";
+	
+?>
+<html>
+<head>
+	<title><?php echo $titulo; ?></title>
+	<meta chasert= "UTF-8" />
+</head>
+<body>
+<?php
+	echo $tituloConteudo;
+	echo $conteudo;
+	?>
+	
+	
